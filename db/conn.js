bab4eefe4bb4ea7f09230564bb40aeafeaefb077
Jagmohan Dixit <78152773+Jagmohan-Dixit@users.noreply.github.com>
2023-03-14 16:15:42 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -14,7 +14,7 @@ const mongoose = require("mongoose");
 // );
 
 
-mongoose.connect("mongodb+srv://jagmohan:UO7Aqp1bEafm2sT9@cluster0.uqqnml2.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect("YOUR_MONGO_URL", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
